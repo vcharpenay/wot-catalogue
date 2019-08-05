480924e779ec32707b55cf0fcf6c4eb4163f82d8
vcharpenay <vcharpenay@gmail.com>
2019-08-05 19:01:55 +0200

    turned wordnet mapping into RDF

--- _script/disambiguate.js
+++ _script/disambiguate.js
@@ -11,19 +11,16 @@ select distinct ?c ?l where {\
 ';
 
 function ngrams(words) {
-    if (words.length == 1) return [words];
+    let all = [];
 
-    let p = words[0] + ' ' + words[1];
+    for (let n = words.length; n > 0; n--) {
+        for (let i = 0; i + n <= words.length; i++) {
+            let ngram = words.slice(i, i + n);
+            all.push(ngram.join(' '));
+        }
+    }
 
-    let ngrams2 = ngrams([p, ...words.slice(2)]);
-    let ngrams1 = ngrams(words.slice(1)).map(ng => [words[0], ...ng]);
-    
-    return [...ngrams2, ...ngrams1];
-}
-
-function byLength(ngram, mgram) {
-    // TODO is [4, 2] better than [3, 3]?
-    return ngram.length - mgram.length;
+    return all;
 }
 
 const txt = fs.readFileSync('../labels.ttl', 'utf-8');
@@ -35,44 +32,48 @@ urdf.load(txt, { format: 'text/turtle' })
 .then(() => urdf.query(q))
 
 .then(res => {
-    labels = res.map(b => b.l.value)
-                .map(l => ngrams(l.split(' ')).sort(byLength));
-
     let promises = res.map(b => {
         let concept = b.c.value;
         let label = b.l.value;
 
-        let sequences = ngrams(label.split(' ')).sort(byLength);
-        return Promise.all(sequences.map(seq => {
-            return Promise.all(seq.map(w => {
-                if (pluralize.isPlural(w)) w = pluralize.singular(w);
-                // TODO process verbs in past tense (-ed + irregular verbs)
-                // TODO same for -ing forms
-        
-                return Promise.all([
-                    wpos.lookupNoun(w),
-                    wpos.lookupVerb(w),
-                    wpos.lookupAdjective(w),
-                    wpos.lookupAdverb(w)
-                ])
-                .then(pos => {
-                    let synset = pos.find(s => s.length > 0) || [];
-                    return synset;
-                });
-            }));
+        return Promise.all(ngrams(label.split(' ')).map(w => {
+            if (pluralize.isPlural(w)) w = pluralize.singular(w);
+            // TODO process verbs in past tense (-ed + irregular verbs)
+            // TODO same for -ing forms
+    
+            return Promise.all([
+                wpos.lookupNoun(w),
+                wpos.lookupVerb(w),
+                wpos.lookupAdjective(w),
+                wpos.lookupAdverb(w)
+            ])
+            .then(pos => {
+                let synset = pos.find(s => s.length > 0) || [];
+                return synset;
+            });
         }))
         .then(synsets => {
-            let seq = synsets.find(seq => seq.some(s => s.length > 0)) || [];
-            return [concept, seq];
+            let choice = synsets.reduce((choice, s) => choice.concat(s), []);
+            return [concept, choice];
         });
     });
 
     return Promise.all(promises);
 })
 
-.then(synsets => {
-    synsets.forEach(([c, s]) => {
-        console.log(c + ' (' + s.length + ')');
-        s.forEach(ss => console.log('\t' + ss.length));
-    });
-});
+.then(concepts => {
+    let skos = 'http://www.w3.org/2004/02/skos/core#';
+    let wnid = 'http://wordnet-rdf.princeton.edu/id/';
+
+    let nt = concepts.reduce((nt, [concept, synsets]) => {
+        return synsets.reduce((nt, s) => {
+            let id = String(s.synsetOffset);
+            id = '00000000'.substring(0, 8 - id.length) + id;
+            return nt + `<${concept}> <${skos}mappingRelation> <${wnid}${id}-${s.pos}> .\n`;
+        }, nt);
+    }, '');
+
+    fs.writeFileSync('../wordnet-mapping.ttl', nt);
+})
+
+.catch(e => console.error(e));
