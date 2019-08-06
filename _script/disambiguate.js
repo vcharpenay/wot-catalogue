a2f7e026c0eb12e279ffb09baa9bcaa98c847bca
vcharpenay <vcharpenay@gmail.com>
2019-08-06 17:47:19 +0200

    implemented greedy disambiguation process, to be tuned

--- _script/disambiguate.js
+++ _script/disambiguate.js
@@ -10,6 +10,28 @@ select distinct ?c ?l where {\
 }\
 ';
 
+function subgraph(concepts, relations) {
+    return concepts.reduce((g, c) => {
+        if (relations[c] && !g[c]) {
+            g[c] = relations[c];
+
+            let sg = subgraph(relations[c], relations);
+            for (let sc in sg) g[sc] = sg[sc];
+        }
+
+        return g;
+    }, {});
+}
+
+// TODO  when considering a full knowledge graph,
+//       should also take the number of edges into account
+//       (here, only number of vertices).
+function fitness(concepts, relations) {
+    let g = subgraph(concepts, relations);
+
+    return 1 / Object.keys(g).length;
+}
+
 function ngrams(words) {
     let all = [];
 
@@ -23,9 +45,28 @@ function ngrams(words) {
     return all;
 }
 
+function wordnetId(synset) {
+    let id = String(synset.synsetOffset);
+    id = '00000000'.substring(0, 8 - id.length) + id;
+    return `http://wordnet-rdf.princeton.edu/id/${id}-${synset.pos}`;
+}
+ 
+const links = fs.readFileSync('all-hypernyms.csv', 'utf-8')
+    .split('\n')
+    .map(r => r.split(',').map(col => col.replace(/"|,| /g, '')))
+    .reduce((idx, [sub, sup]) => {
+        if (!idx[sub]) idx[sub] = [];
+        idx[sub].push(sup);
+        return idx;
+    }, {});
+
 const txt = fs.readFileSync('../labels.ttl', 'utf-8');
 
 const wpos = new WordPOS();
+
+const senses = {};
+
+const denotations = {};
 
 urdf.load(txt, { format: 'text/turtle' })
 
@@ -40,6 +81,11 @@ urdf.load(txt, { format: 'text/turtle' })
             if (pluralize.isPlural(w)) w = pluralize.singular(w);
             // TODO process verbs in past tense (-ed + irregular verbs)
             // TODO same for -ing forms
+
+            if (!denotations[w]) denotations[w] = [];
+            denotations[w].push(concept);
+
+            if (senses[w]) return Promise.resolve();
     
             return Promise.all([
                 wpos.lookupNoun(w),
@@ -48,28 +94,40 @@ urdf.load(txt, { format: 'text/turtle' })
                 wpos.lookupAdverb(w)
             ])
             .then(pos => {
-                let synset = pos.find(s => s.length > 0) || [];
-                return synset;
+                // selects only one POS, by preference (n, v, s, a)
+                let synsets = pos.find(s => s.length > 0) || [];
+                senses[w] = synsets.map(wordnetId);
             });
-        }))
-        .then(synsets => {
-            let choice = synsets.reduce((choice, s) => choice.concat(s), []);
-            return [concept, choice];
-        });
+        }));
     });
 
     return Promise.all(promises);
 })
 
-.then(concepts => {
-    let skos = 'http://www.w3.org/2004/02/skos/core#';
-    let wnid = 'http://wordnet-rdf.princeton.edu/id/';
+.then(() => {
+    let labels = Object.keys(senses)
+        .filter(l => senses[l].length > 0)
+        .sort((l1, l2) => senses[l1].length - senses[l2].length);
+        
+    // greedy algorithm
+    // TODO disambiguate per concept scheme, not globally
+    let disambiguated = labels.reduce((dis, l) => {
+        let prev = Object.values(dis);
 
-    let nt = concepts.reduce((nt, [concept, synsets]) => {
-        return synsets.reduce((nt, s) => {
-            let id = String(s.synsetOffset);
-            id = '00000000'.substring(0, 8 - id.length) + id;
-            return nt + `<${concept}> <${skos}mappingRelation> <${wnid}${id}-${s.pos}> .\n`;
+        let best = senses[l].map(s => [s, fitness([...prev, s], links)])
+                 .sort((f1, f2) => f2[1] - f1[1])
+                 [0][0];
+
+        dis[l] = best;
+
+        return dis;
+    }, {});
+
+    let skos = 'http://www.w3.org/2004/02/skos/core#';
+
+    let nt = labels.reduce((nt, l) => {
+        return denotations[l].reduce((nt, c) => {
+            return nt + `<${c}> <${skos}mappingRelation> <${disambiguated[l]}> .\n`;
         }, nt);
     }, '');
 
