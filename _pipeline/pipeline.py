5813b752fb19984307c74f08aea4881f1eb764a1
Victor Charpenay <victor.charpenay@emse.fr>
2024-07-26 15:34:32 +0200

    generalize pipeline script

--- _pipeline/pipeline.py
+++ _pipeline/pipeline.py
@@ -1,35 +1,32 @@
+from pathlib import Path
 from traceback import print_exc
 from rdflib import Graph
 from lifting import lift
 
-# TODO browse folders, look for <collection>.source
+wd = Path(".")
 
-# scheme = "lwm2m"
-# collection = "ipso"
+def process_source(scheme, collection):
+    with open(f"{scheme}/{collection}.source", "r") as sources:
+        g = Graph()
 
-# scheme = "ble"
-# collection = "gatt"
-
-# scheme = "bacnet"
-# collection = "core"
-
-# scheme = "onem2m"
-# collection = "haim"
-
-scheme = "ocf"
-collection = "oneiota"
-
-with open(f"{scheme}/{collection}.source", "r") as sources:
-    g = Graph()
-
-    for uri in sources:
-        try:
-            uri = uri.replace("\n", "")
-            coll = lift(uri, scheme)
-            for t in coll: g.add(t)
-            for prefix, ns in coll.namespaces(): g.bind(prefix, ns)
-        except:
-            print("Failed to process source: ", uri)
-            print_exc()
+        for uri in sources:
+            try:
+                uri = uri.replace("\n", "")
+                coll = lift(uri, scheme)
+                for t in coll: g.add(t)
+                for prefix, ns in coll.namespaces(): g.bind(prefix, ns)
+            except:
+                print("Failed to process source: ", uri)
+                print_exc()
 
     g.serialize(destination=f"{scheme}/{collection}.ttl")
+
+for dir in wd.iterdir():
+    if dir.is_dir():
+        for f in dir.iterdir():
+            if f.name.endswith(".source"):
+                scheme = dir.name
+                collection = f.name.replace(".source", "")
+
+                print(f"Processing {collection} in folder {scheme}")
+                process_source(scheme, collection)
